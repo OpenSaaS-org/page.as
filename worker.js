ad205ac526f38fd3559e5a10d1c538152d41eccc
Nathan Clevenger <nateclev@gmail.com>
2022-07-23 07:55:49 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,7 +1,6 @@
 export default {
   fetch: req => {
     const url = new URL(req.url)
-    if (url.pathname == '/') return fetch('https://longtailstudio.github.io/page.as')
     const [user] = url.hostname.split('.')
     url.hostname = user + '.github.io'
     return fetch(url.toString(), req)
