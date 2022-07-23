bdadc324a14bd42f0dc1a64db322f7b6d0244996
Nathan Clevenger <nateclev@gmail.com>
2022-07-23 08:10:12 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -3,6 +3,10 @@ export default {
     const url = new URL(req.url)
     const [user] = url.hostname.split('.')
     url.hostname = user + '.github.io'
-    return fetch(url.toString(), req)
+    const res = await fetch(url.toString(), req)
+    const newRequest = new Request(url.toString(), {
+        redirect: 'follow',
+    })
+    return fetch(newRequest)
   }
 }
